48a6b32bbee7f21b71d37003a75f321b8d86b24d
BangSann <yosanokta12@gmail.com>
2024-02-23 21:14:44 +0700

    Commit 4

---
+++ assets/index.js
@@ -0,0 +1,18 @@
+var person = {
+  nama: "yosan okta",
+  age: 21,
+  universitas: "UNP Kediri",
+  jurusan: "Teknik Informatika",
+};
+
+console.log(
+  "nama saya " +
+    person.nama +
+    " , umur saya " +
+    person.age +
+    " tahun , saya kuliah di " +
+    person.universitas +
+    " mengambil jurusan " +
+    person.jurusan +
+    "."
+);
