b6c188f49560dfb4b0ad644fbed39eadc5c4d380
BangSann <yosanokta12@gmail.com>
2024-02-28 18:46:18 +0700

    feat: Add js file for suit game

--- assets/index.js
+++ assets/index.js
@@ -1,18 +1,63 @@
-var person = {
-  nama: "yosan okta",
-  age: 21,
-  universitas: "UNP Kediri",
-  jurusan: "Teknik Informatika",
+let pilihan1 = document.getElementById("option1");
+let pilihan2 = document.getElementById("option2");
+let pilihan3 = document.getElementById("option3");
+
+pilihan1.addEventListener("click", () => {
+  // alert(pilihan1.value)
+  let randNum = Math.random();
+  suit(pilihan1.value, pilihanLawan(randNum));
+  document.getElementById("option").innerText = pilihan1.value;
+  document.getElementById("lawan").innerText = pilihanLawan(randNum);
+});
+pilihan2.addEventListener("click", () => {
+  // alert(pilihan2.value)
+  let randNum = Math.random();
+  suit(pilihan2.value, pilihanLawan(randNum));
+  document.getElementById("option").innerText = pilihan2.value;
+  document.getElementById("lawan").innerText = pilihanLawan(randNum);
+});
+pilihan3.addEventListener("click", () => {
+  // alert(pilihan3.value)
+  let randNum = Math.random();
+  suit(pilihan3.value, pilihanLawan(randNum));
+  document.getElementById("option").innerText = pilihan3.value;
+  document.getElementById("lawan").innerText = pilihanLawan(randNum);
+});
+
+const pilihanLawan = (randNum) => {
+  if (randNum > 0.6) {
+    return "Batu";
+  } else if (randNum > 0.3) {
+    return "Kertas";
+  } else {
+    return "Gunting";
+  }
 };
 
-console.log(
-  "nama saya " +
-    person.nama +
-    " , umur saya " +
-    person.age +
-    " tahun , saya kuliah di " +
-    person.universitas +
-    " mengambil jurusan " +
-    person.jurusan +
-    "."
-);
+const suit = (option, lawan) => {
+  if (option == lawan) {
+    document.getElementById('hasil').innerText = "draw"
+  } else {
+    if (option == "Gunting") {
+      if (lawan == "Kertas") {
+        document.getElementById('hasil').innerText = "menang"
+      } else if (lawan == "Batu") {
+        document.getElementById('hasil').innerText = "kalah"
+      }
+    }
+    else if (option == "Batu") {
+      if (lawan == "Kertas") {
+        document.getElementById('hasil').innerText = "kalah"
+      } else if (lawan == "Gunting") {
+        document.getElementById('hasil').innerText = "menang"
+      }
+    }
+    else if (option == "Kertas") {
+      if (lawan == "Gunting") {
+        document.getElementById('hasil').innerText = "kalah"
+      } else if (lawan == "Batu") {
+        document.getElementById('hasil').innerText = "menang"
+      }
+    }
+  }
+};
